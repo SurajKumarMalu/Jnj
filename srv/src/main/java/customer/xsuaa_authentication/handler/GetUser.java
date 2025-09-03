5e9b31ee26ab666b04992b06fbc8bd685d07b6e4
Suraj Malu <suraj.kumar@incture.com>
2025-09-03 06:18:08 +0000

    Sync Commit @9/3/2025, 6:18:08 AM

---
+++ srv/src/main/java/customer/xsuaa_authentication/handler/GetUser.java
@@ -0,0 +1,34 @@
+package customer.xsuaa_authentication.handler;
+
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.stereotype.Component;
+
+import com.sap.cds.services.cds.CdsReadEventContext;
+import com.sap.cds.services.handler.EventHandler;
+import com.sap.cds.services.handler.annotations.On;
+import com.sap.cds.services.handler.annotations.ServiceName;
+import com.sap.cds.services.request.UserInfo;
+
+@Component
+@ServiceName("UserService")
+public class GetUser implements EventHandler {
+    
+    private final UserInfo userInfo ;
+
+    public GetUser(UserInfo userInf) {
+        this.userInfo = userInf;
+    }
+     @On(event = "getCurrentUser")
+    public void onGetCurrentUser(CdsReadEventContext context) {
+        Map<String, Object> currentUser = Map.of(
+            "id", userInfo.getName(),       // unique user ID
+           // "email", userInfo.getEmail(),   // email from token
+            "tenant", userInfo.getTenant(), // subaccount tenant
+            "roles", userInfo.getRoles()    // assigned role names
+        );
+        context.setResult(List.of(currentUser));
+    }
+
+}
